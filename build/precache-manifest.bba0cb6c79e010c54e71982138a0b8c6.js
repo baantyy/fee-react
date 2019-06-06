96928797c21b10bf495b041e2644b89bc8e53e57
Biswajeet Maharana <33840899+baantyy@users.noreply.github.com>
2019-06-06 17:21:00 +0530

    Add files via upload

---
+++ build/precache-manifest.bba0cb6c79e010c54e71982138a0b8c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eced93e609feacbf14ee38659b3daa5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8411aa129aead59feaf7",
+    "url": "/static/css/main.08df9a27.chunk.css"
+  },
+  {
+    "revision": "b77ecb897421cad52c82",
+    "url": "/static/js/2.ff73b990.chunk.js"
+  },
+  {
+    "revision": "8411aa129aead59feaf7",
+    "url": "/static/js/main.0133815c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
